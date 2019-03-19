4e89daf1db1c39130415ee760bdc66876bb78939
Hussein-A <43759836+Hussein-A@users.noreply.github.com>
2019-03-19 16:38:50 -0400

    Add make_heap and heapsort
    Added a make heap constructor in contrast to using insert n times. This is optimized to be bounded above by nlogn using bubble down. For more details see make_heap or pg 113 of Skienna, Algorithm Design Manual.
    
    Also added heapsort.

--- Heap.cpp
+++ Heap.cpp
@@ -6,7 +6,6 @@
 
 //implementation of (min)heap for integers, using array representation
 //position of the children of k are 2*k+1 and 2*k+2
-	std::vector<int> priority_queue;
 
 	int parent(std::vector<int>& pq, const int pos) { 
 		if (pos == 0) return -1;
@@ -23,10 +22,7 @@
 		if (child_pos >= (int)pq.size()) return -1;
 		else return child_pos;
 	}
-	int get_min(std::vector<int>& pq) {
-		if (pq.size() <= 0) std::cout << "Warning no elements in queue!";
-		else return pq[0];
-	}
+
 
 
 void bubble_up(std::vector<int>& pq, const int pos) {
@@ -72,13 +68,36 @@ void delete_min(std::vector<int>& pq) {
 	bubble_down(pq, 0);
 }
 
+int get_min(std::vector<int>& pq) {
+	if (pq.size() <= 0) std::cout << "Warning no elements in queue!";
+	else {
+		int temp = pq[0];
+		delete_min(pq);
+		return temp;
+	}
+}
+
+std::vector<int> make_heap(std::vector<int>& v) {
+	//utilize the bubble down procedure of creating a heap by
+	//bubble down from the end, rather than insert one by one.
+	std::vector<int> priority_queue = v;
+	int last = priority_queue.size() - 1;
+	for (int i = last; i >= 0; --i) {
+		bubble_down(priority_queue, i);
+	}
+	return priority_queue;
+}
+
+void heapsort(std::vector<int>& v) {
+	std::vector<int> priority_queue = make_heap(v);
+	for (int i = 0; i < v.size(); ++i) {v[i] = get_min(priority_queue);}
+}
+
 
 
 int main() {
-	std::vector<int> test;
-	std::vector<int> values{ 1492,1783,1804,1918,2001,1865,1941,1776,1945,1963 };
-	for (int x : values) { insert(test, x); }
-	delete_min(test);
+	std::vector<int> test{ 1492,1783,1804,1918,2001,1865,1941,1776,1945,1963, 2111,77, -11 };
+	heapsort(test);
 
 	for (int x : test) std::cout << x << "\n";
 }
