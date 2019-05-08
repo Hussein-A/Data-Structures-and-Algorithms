1ce9b26a6ffb2bcf3e76810285d8a18683413410
Hussein-A <43759836+Hussein-A@users.noreply.github.com>
2019-05-08 17:35:11 -0400

    add read_file function, remove namespace std
    Added an option to read a graph from a text file using specific file structure to avoid having to re-enter graphs each time for testing algorithms. Also removed use of namespace std to avoid conflict.

--- Graphs/graph.h
+++ Graphs/graph.h
@@ -4,21 +4,20 @@
 #pragma once
 #include<vector>
 
-using namespace std;
-
 struct edgenode {
 	int neighbor;
 	int weight{ 1 };
 };
 
 struct graph {
-	vector<vector<edgenode>> edges;
-	vector<int> degree;
+	std::vector<std::vector<edgenode>> edges;
+	std::vector<int> degree;
 	int nvertices{ 0 };
 	int nedges{ 0 };
 	bool directed{ false };
 
 	void read_graph();
+	void read_file(const std::string& name);
 	void insert_edge(int x, int y, bool directed, int weight = 1);
 	void print_graph();
 };
